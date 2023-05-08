ceab105cabdf379d8e35cad69cf91e7a2498fb86
MuthuramanChockalingam <ramsmart710@gmail.com>
2023-05-08 18:10:10 +0530

    Modified response.

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
 app.get('/json', (req, res) => process.env.MESSAGE_STYLE === "uppercase"
-    ? res.json({"message": "Hello json"}).toUpperCase()
+    ? res.json({"message": "HELLO JSON"})
     : res.json({"message": "Hello json"})
 );
 
