a772d77b2193ca98b96465c2565140a22cfe4a6c
MuthuramanChockalingam <ramsmart710@gmail.com>
2023-05-08 16:56:06 +0530

    Added env variable in request.

--- myApp.js
+++ myApp.js
@@ -1,11 +1,15 @@
 let express = require('express');
 let app = express();
+let process = require('dotenv').config();
 
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
-app.get('/json', (req, res) => res.json({"message": "Hello json"}));
+app.get('/json', (req, res) => process.env.MESSAGE_STYLE === uppercase
+    ? res.json({"message": "Hello json"}).toUpperCase()
+    : res.json({"message": "Hello json"})
+);
 
 
 
