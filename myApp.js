91580f52ee134140417ebe89219db25869f92c97
MuthuramanChockalingam <ramsmart710@gmail.com>
2023-05-08 15:50:09 +0530

    Updated absolute path.

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 
-app.get('/', (req, res) => res.sendFile('./views/index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
 
 
