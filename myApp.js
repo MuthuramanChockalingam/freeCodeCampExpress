ff9671135b0655dfb6fb231d3bbad36f22ce22ac
MuthuramanChockalingam <ramsmart710@gmail.com>
2023-05-08 16:28:45 +0530

    Added public route.

--- myApp.js
+++ myApp.js
@@ -1,8 +1,9 @@
 let express = require('express');
 let app = express();
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
 
 
