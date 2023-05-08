671cea0f8bfaa555d957e6875b5d1e534ddcd9df
MuthuramanChockalingam <ramsmart710@gmail.com>
2023-05-08 18:02:42 +0530

    Fixed more bug.

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
-app.get('/json', (req, res) => process.env.MESSAGE_STYLE === uppercase
+app.get('/json', (req, res) => process.env.MESSAGE_STYLE === "uppercase"
     ? res.json({"message": "Hello json"}).toUpperCase()
     : res.json({"message": "Hello json"})
 );
